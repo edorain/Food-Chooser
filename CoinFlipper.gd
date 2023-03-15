0f958224d51a5f1b59feef169253a98eac012168
LEE HAW HUA <LS0593@lemonsky.com>
2023-03-15 14:17:24 +0800

    Lovely

--- CoinFlipper.gd
+++ CoinFlipper.gd
@@ -1,6 +1,11 @@
 extends Control
 
+# Setting the buttons type
+enum button_types {CoinFlipper,RandomFood}
+@export var button_function = button_types.CoinFlipper
+
 enum sides {GONG,HUA}
+var food_types = ["Noodle","Rice","Bread"]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,6 +18,18 @@ func _process(delta):
 
 
 func _on_button_pressed():
-	var randomness = randi_range(0,1)
-	var flipped = sides.find_key(randomness)
-	$Label.text = sides[flipped] # Replace with function body.
+	# Coin Flipper Function
+	if button_function == button_types.CoinFlipper :
+		var randomness = randi_range(0,sides.size()-1)
+		var flipped = sides.find_key(randomness)
+		_displaythetext(flipped)
+	elif button_function == button_types.RandomFood :
+		_displaythetext(food_types.pick_random())
+
+
+func _displaythetext(answer):
+	$Label.text = answer # Replace with function body.
+
+
+func _on_line_edit_text_submitted(new_text):
+	pass # Replace with function body.
